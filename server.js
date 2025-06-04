cda0704f28a2946a55b1c7657d102ff0dc746a90
Ndutech1 <136145477+Ndutech1@users.noreply.github.com>
2025-06-04 04:20:36 -1100

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
+import cookieparser from 'cookie-parser':
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-
+app.use(cors({
+  origin: 'https://movie-app-frontend-rye9.vercel.app/',
+  credentials: true,
+}));
+res.cookie('token', token, {
+  httpOnly: true,
+  secure: true,         // ✅ Important for production (HTTPS)
+  sameSite: 'None',     // ✅ Required for cross-site cookies
+  maxAge: 24 * 60 * 60 * 1000 // 1 day
+});
 app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
